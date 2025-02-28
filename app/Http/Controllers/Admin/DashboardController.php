d12b89bf8f7ff86d4d488805d4e68721b4cfb2a3
shahkavita1996@gmail.com <shahkavita1996@gmail.com>
2025-02-28 13:01:46 +0530

    auth issue solve

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class DashboardController extends Controller
 {
     public function index()
     {
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Admin\DashboardController;
 Auth::routes();
 
 Route::any('/', [LoginController::class, 'showLoginForm'])->name('admin');
@@ -11,6 +11,6 @@ Route::any('/admin/login', [LoginController::class, 'showLoginForm'])->name('adm
 Route::any('/check/login', [LoginController::class, 'login']);
 
 
-Route::middleware(['admin'])->prefix('admin')->group(function () {
-    Route::get('/home', [App\Http\Controllers\Admin\HomeController::class, 'index'])->name('admin.home');
+Route::middleware(['auth'])->prefix('admin')->group(function () {
+    Route::get('/home', [DashboardController::class, 'index'])->name('admin.home');
 });
