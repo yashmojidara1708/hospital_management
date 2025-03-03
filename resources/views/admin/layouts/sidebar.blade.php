5cbd38859b2cac0086f363d8188ea779cacfd7f7
Nikunj <Nikunj>
2025-03-03 17:47:24 +0530

    conflict solve

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,6 +1,6 @@
 @php
     $currentRouteName = \Route::currentRouteName();
-      
+
 @endphp
 
 <!-- Sidebar -->
@@ -8,33 +8,17 @@
     <div class="sidebar-inner slimscroll">
         <div id="sidebar-menu" class="sidebar-menu">
             <ul>
-                @if(session('user_role_name')=='Admin' || session('user_role_name') == 'Doctor' 
-                || session('user_role_name') == 'Receptionist')
                 <!--admin sidebar-->
-                        <li class="menu-title">
-                           <span><h5>{{ session('user_role_name', 'N/A') }}</h5>
-                           </span>
-                        </li>
-                        <li class="menu-item  {{ $currentRouteName == 'admin.home' ? 'active' : '' }}">
-                            <a href="{{ route('admin.home') }}" class="menu-link">
-                                <i class='fe fe-home'></i>
-                                <span>Dashboard</span>
-                                
-                            </a>
-                        </li>
-                
+                <li class="menu-title">
+                    <span>
+                        <h5>{{ session('user_role_name', 'N/A') }}</h5>
+                    </span>
+                </li>
 
                 <li class="menu-item  {{ $currentRouteName == 'admin.home' ? 'active' : '' }}">
                     <a href="{{ route('admin.home') }}" class="menu-link">
                         <i class='menu-icon fa-solid fa-home'></i>
                         <span>Dashboard</span>
-                    </a>
-                </li>
-
-                <li class="menu-item  {{ $currentRouteName == 'admin.specialities' ? 'active' : '' }}">
-                    <a href="{{ route('admin.specialities') }}" class="menu-link">
-                        <i class='menu-icon fa-solid fa-stethoscope'></i>
-                        <span>Specialities</span>
                     </a>
                 </li>
 
@@ -50,18 +34,11 @@
                         <i class="menu-icon fa-solid fa-hospital-user"></i>
                         <span>Patients</span>
                     </a>
-                    </li>
-                    <li>
-                        <a href="appointment-list.html"><i class="fe fe-layout"></i> <span>Appointments</span></a>
-                    </li>
-              @endif 
-                <!--admin sidebar-->   
-          @if(session('user_role_name')== 'Admin') 
-                <li class="menu-item  {{ $currentRouteName == 'admin.role' ? 'active' : '' }}">
-                    <a href="{{ route('admin.role') }}" class="menu-link">
-                        <i class='fe fe-users'></i>
-                        <span>HMS Role</span>
                 </li>
+                <li>
+                    <a href="appointment-list.html"><i class="fe fe-layout"></i> <span>Appointments</span></a>
+                </li>
+
                 <li class="menu-item  {{ $currentRouteName == 'admin.medicines' ? 'active' : '' }}">
                     <a href="{{ route('admin.medicines') }}" class="menu-link">
                         <i class="menu-icon fa-solid fa-pills"></i>
@@ -72,10 +49,8 @@
                     <a href="{{ route('admin.staff') }}" class="menu-link">
                         <i class="menu-icon fa-solid fa-users"></i>
                         <span>Staff</span>
-
                     </a>
                 </li>
-        @endif
             </ul>
         </div>
     </div>
